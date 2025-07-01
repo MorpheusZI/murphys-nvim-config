314587cda794fb70e87651468d44edaadbbbbcb6
MorpheusZI <MorpheusZI>
2025-07-01 14:08:14 +0700

    time based colorscheme

--- lua/plugins/ColorSchemes/avimcolorschemes.lua
+++ lua/plugins/ColorSchemes/avimcolorschemes.lua
@@ -3,6 +3,22 @@ return {
   priority = 1000,
   lazy = false,
   init = function()
-    vim.cmd [[colorscheme gotham]]
+    local function set_time_based_colorscheme()
+      local current_hour = tonumber(os.date '%H')
+
+      if current_hour >= 20 then -- 8pm (20:00) or later
+        vim.cmd [[colorscheme alduin]]
+      else
+        vim.cmd [[colorscheme citruszest]]
+      end
+    end
+
+    set_time_based_colorscheme()
+
+    vim.api.nvim_create_autocmd('CursorHold', {
+      callback = function()
+        set_time_based_colorscheme()
+      end,
+    })
   end,
 }
--- lua/plugins/codesnap.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'mistricky/codesnap.nvim',
-  build = 'make',
-  opts = {
-    watermark = 'edi',
-  },
-}
